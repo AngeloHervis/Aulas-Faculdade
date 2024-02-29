5a6b514ea4fee3973f30aaa3b98b4b7fa581bd6d
Angelo Hervis <angelo.hervis@gmail.com>
2024-02-29 20:28:26 -0300

    att

--- JAVA/Aula 02/aula2/src/App.java
+++ JAVA/Aula 02/aula2/src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        /*
+         * next() - Serve pra receber uma Palavra
+         * nextLine() - Serve pra receber um Texto
+         * nextInt() - Serve pra receber um Inteiro
+         * nextLong() - Serve pra receber um long
+         * nextFloat() - Serve pra receber um Float
+         * nextBoolean() - Serve pra receber um Booleano
+         */
+
+         Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Aula de Estruturas Condicionais");
+
+        soma = a + b;
+        subtracao = b - a;
+        multiplicacao = a * b;
+        divisao = b / a;
+        restoDivisao = b % a;
+
+        System.out.println("Soma = " + soma);
+        System.out.println("Subtração = " + subtracao);
+        System.out.println("Multipliocação = " + multiplicacao);
+        System.out.println("Divisão = " + divisao);
+        System.out.println("Resto da Divisão = " + restoDivisao);
+
+        // Incrementos e Decrementos
+
+        System.out.println("Incremento após = " + a++);
+        System.out.println("Incremento antes = " + ++b);
+
+        System.out.println("Decremento após = " + a--);
+        System.out.println("Decremento antes = " + --b);
+
+        
+    } 
 }
