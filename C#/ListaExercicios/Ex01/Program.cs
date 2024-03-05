0084dcf93010bc8f9ab9ddf18ffbbc0856903ddb
Angelo <angelo.hervis@gmail.com>
2024-03-04 21:44:51 -0300

    att

--- C#/ListaExercicios/Ex01/Program.cs
+++ C#/ListaExercicios/Ex01/Program.cs
@@ -8,8 +8,34 @@ class Program
         Random random = new Random();
         for (int i = 0; i < desordenado.Length; i++)
         {
-            desordenado[i] = random.Next[100];
+            desordenado[i] = random.Next(100);
         }
-        Console.WriteLine(random.Next(100));
+
+        Console.WriteLine("Array desordenado:");
+        for (int i = 0; i < desordenado.Length; i++)
+        {
+            Console.Write(desordenado[i] + " ");
+        }
+
+        // Bubble Sort
+        int n = desordenado.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (desordenado[j] > desordenado[j + 1])
+                {
+                    int temp = desordenado[j];
+                    desordenado[j] = desordenado[j + 1];
+                    desordenado[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("\nArray ordenado:");
+        for (int i = 0; i < desordenado.Length; i++)
+        {
+            Console.Write(desordenado[i] + " ");
+        }
     }
 }
