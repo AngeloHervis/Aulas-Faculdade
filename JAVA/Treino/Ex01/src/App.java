fde303dc6ac9bf1ea2a41efef81966a72e7c08bd
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-03-20 19:12:50 -0300

    att

---
+++ JAVA/Treino/Ex01/src/App.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Calculadora Simples");
+        System.out.println("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Digite a operação desejada (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+
+        double resultado = 0.0;
+
+        switch (operacao) {
+            case '+':
+                resultado = num1 +num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if(num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    System.out.println("Erro: divisão por zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                return;
+        }
+        System.out.println("Resultado:" + resultado);
+
+        scanner.close();
+    }
+}
