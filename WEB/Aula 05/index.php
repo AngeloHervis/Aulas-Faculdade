3ba11857c472f2101eb7261c0747596813d74626
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-05 20:42:18 -0300

    att

--- WEB/Aula 05/index.php
+++ WEB/Aula 05/index.php
@@ -49,9 +49,85 @@
     echo "Resultado 1: $resultado1 <br>";
     echo "Resultado 2: $resultado2 <br>";
     //Exercício 03
+    function calcularOperacao($operacao, $numero1, $numero2)
+    {
+        switch ($operacao) {
+            case 'soma':
+                $resultado = $numero1 + $numero2;
+                echo "Resultado da soma: $resultado";
+                break;
+            case 'subtracao':
+                $resultado = $numero1 - $numero2;
+                echo "Resultado da subtração: $resultado";
+                break;
+            case 'multiplicacao':
+                $resultado = $numero1 * $numero2;
+                echo "Resultado da multiplicação: $resultado";
+                break;
+            case 'divisao':
+                if ($numero2 != 0) {
+                    $resultado = $numero1 / $numero2;
+                    echo "Resultado da divisão: $resultado";
+                } else {
+                    echo "Não é possível dividir por zero.";
+                }
+                break;
+            case 'exponenciacao':
+                $resultado = pow($numero1, $numero2);
+                echo "Resultado da exponenciação: $resultado";
+                break;
+            default:
+                echo "Operação inválida.";
+                break;
+        }
+    }
+
+    if (isset ($_POST['submit'])) {
+        $operacao = $_POST['operacao'];
+        $numero1 = $_POST['numero1'];
+        $numero2 = $_POST['numero2'];
+        calcularOperacao($operacao, $numero1, $numero2);
+    }
+
+
+
+
     //Exercício 04
+    function calcularMediaAluno($notas_aluno)
+    {
+        $soma_notas = array_sum($notas_aluno);
+        $quantidade_notas = count($notas_aluno);
+        $media_aluno = $soma_notas / $quantidade_notas;
+
+        if ($media_aluno > 10) {
+            echo "Alguma(s) das notas digitadas pelo usuário é inválida.";
+        } elseif ($media_aluno >= 6) {
+            echo "Aluno aprovado! Média final: $media_aluno";
+        } else {
+            echo "Aluno reprovado! Média final: $media_aluno";
+        }
+    }
+
+    // Exemplo de uso da função
+    $notas_aluno1 = array(7, 8, 6, 9);
+    calcularMediaAluno($notas_aluno1);
     //Exercício 05
     ?>
+
+    <form method="post" action="">
+        Escolha a operação:
+        <select name="operacao">
+            <option value="soma">Soma</option>
+            <option value="subtracao">Subtração</option>
+            <option value="multiplicacao">Multiplicação</option>
+            <option value="divisao">Divisão</option>
+            <option value="exponenciacao">Exponenciação</option>
+        </select>
+        <br>
+        Digite o primeiro número: <input type="number" name="numero1"><br>
+        Digite o segundo número: <input type="number" name="numero2"><br>
+        <input type="submit" name="submit" value="Calcular">
+    </form>
 
 </body>
 
