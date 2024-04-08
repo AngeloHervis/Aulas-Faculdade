bf8a0b7e36cd05985f98c52faf0cf4bd3a6af254
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-04-08 19:23:25 -0300

    att

---
+++ C#/API/API/Program.cs
@@ -0,0 +1,68 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<Produto> produtos = new List<Produto>();
+
+// Endpoints = Funcionalidades - JSON
+// POST: http://localhost:5076/api/produto/cadastrar
+app.MapPost("/api/produto/cadastrar", ([FromBody] Produto produto) =>
+{
+    //Adicionando o produto dentro da lista
+    produtos.Add(produto);
+    return Results.Created($"/api/produto/buscar/{produto.Id}", produto);
+});
+
+// GET: http://localhost:5076/api/produto/listar
+app.MapGet("/api/produto/listar", () => produtos);
+
+// GET: http://localhost:5076/api/produto/buscar/{iddoproduto}
+app.MapGet("/api/produto/buscar/{id}", ([FromRoute] string id) =>
+{
+    //Endpoint com várias linhas de código
+    for (int i = 0; i < produtos.Count; i++)
+    {
+        if (produtos[i].Id == id)
+        {
+            return Results.Ok(produtos[i]);
+        }
+    }
+    return Results.NotFound("Produto não encontrado!");
+});
+
+// DELETE: http://localhost:5076/api/produto/deletar/{iddoproduto}
+app.MapDelete("/api/produto/deletar/{id}", ([FromRoute] string id) =>
+{
+    //Endpoint com várias linhas de código
+    for (int i = 0; i < produtos.Count; i++)
+    {
+        if (produtos[i].Id == id)
+        {
+            produtos.Remove(produtos[i]);
+            return Results.NoContent();
+        }
+    }
+    return Results.NotFound("Produto não encontrado!");
+});
+
+
+// PUT: http://localhost:5076/api/produto/alterar/{iddoproduto}
+app.MapPut("/api/produto/alterar/{id}", ([FromRoute] string id, [FromBody] Produto produtoAlterado) =>
+{
+    //Endpoint com várias linhas de código
+    for (int i = 0; i < produtos.Count; i++)
+    {
+        if (produtos[i].Id == id)
+        {
+            produtos[i].Nome = produtoAlterado.Nome;
+            produtos[i].Descricao = produtoAlterado.Descricao;
+            produtos[i].Preco = produtoAlterado.Preco;
+            return Results.Ok("Produto alterado com sucesso!");
+        }
+    }
+    return Results.NotFound("Produto não encontrado!");
+});
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,3 @@
+# Aulas-de-análise-e-desenvolvimento-de-sistemas-3°-Semestre
+
+Repositório com os projetos do curso de ADS da Universidade Positivo.
